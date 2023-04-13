246ce06a9e69058cd9db351e9437620f1e2c7c74
Travis Wagner <travis.wagner@umusic.com>
2023-04-13 16:24:58 -0400

    update with crablange content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'CrabLang';
+export const SITE_DESCRIPTION = 'A community fork of a language named after a plant fungus.';
