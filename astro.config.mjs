e74f914a3f9da6ac7f573bc6cf61ce8096f44d79
Travis Wagner <travis.wagner@umusic.com>
2023-04-20 01:00:40 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://crablang.org',
-	integrations: [mdx(), sitemap()],
+  site: 'https://install.crablang.org',
+  integrations: [mdx(), sitemap()],
+  output: 'server',
+  adapter: vercel()
 });
